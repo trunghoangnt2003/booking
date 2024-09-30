b1a386c8f7e67fe4a566eef4c1247c3789f57849
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-09-30 20:42:06 +0700

    env

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,13 @@
-export const Home = () =>{
+import clsx from 'clsx';
+type HomeProps = {
+    classes?: {
+        [key: string]: string;
+    };
+};
+export const Home: React.FC<HomeProps> = ({classes}) =>{
     return (
-        <>
+        <div className={clsx(classes?.container,"text-red-400")}>
             Home
-        </>
+        </div>
     )
 }
--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,1 +1,5 @@
-export * from './home'
+import { withClasses } from "../../common"
+import { Home as Hm } from "./home"
+import classes from "./home.module.scss"
+
+export const Home = withClasses(classes, Hm)
